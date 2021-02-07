fbcb884fbaa7054043374934dd2ee24124d7d014
dingdang <weihagu_work@163.com>
2021-02-07 15:22:18 +0800

    al

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var objects = {}
+var vl = 3000;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
@@ -18,15 +19,26 @@ var sceneAdd = () => {
 	//画点
 	var pointsgroup = new THREE.Group();
 	scene.add(pointsgroup);
-	var geometry = new THREE.SphereGeometry(50, 500, 500);
-	var vl = geometry.vertices.length;
+	var geometry = new THREE.SphereGeometry(20, 100, 100);
+	var gvl = geometry.vertices.length;
+	console.log(geometry.vertices);
+	var material2 = new THREE.MeshLambertMaterial({
+	 color: 0xff00ff
+	});
+	var mesh2 = new THREE.Mesh(geometry, material2); //网格模型对象Mesh
+	mesh2.translateY(120); //球体网格模型沿Y轴正方向平移120
+	scene.add(mesh2);
+	// 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
+	var axisHelper = new THREE.AxesHelper(250);
+	scene.add(axisHelper);
+	//geometry.position.y=10;
 	for (var k = 0; k < vl; k++) {
 		//为每个点附上材质
 		var geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
 		// 点渲染模式
 		var material = new THREE.PointsMaterial({
 			color: Math.random() * 0x808008 + 0x808080,
-			size: 0.4 //点对象像素尺寸
+			size: 0.2 //点对象像素尺寸
 		}); //材质对象
 		var particle = new THREE.Points(geometry, material); //点模型对象
 		particle.position.x = 0;
@@ -35,10 +47,11 @@ var sceneAdd = () => {
 		pointsgroup.add(particle);
 	}
 
-	var i = 0
 	try{
+	var gi = 0;
 	pointsgroup.traverse(function(child) {
 		var timerandom = 1 * Math.random();
+		var i=gi%gvl;
 		if(i<=geometry.vertices.length){
 		TweenMax.to(
 			child.position,
@@ -46,10 +59,10 @@ var sceneAdd = () => {
 				x: geometry.vertices[i]['x'] + (0.5 - Math.random()) * 100,
 				y: geometry.vertices[i]['y'] + (0.5 - Math.random()) * 100,
 				z: geometry.vertices[i]['z'] + Math.random() * 100,
-				delay: 3
+				delay: 2.5
 			}
 		);
-		i++;
+		gi++;
 		}
 
 	});
