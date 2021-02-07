8cf86ae604bf26a10bdc68a1126ad5561cc5cf59
dingdang <weihagu_work@163.com>
2021-02-07 14:38:02 +0800

    使用js算了，webpack比较诡异，报的错

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
-import * as THREE from 'three'
-import {
-	scene,
-	camera,
-	renderer
-} from './scene'
-import {
-	TweenMax
-} from 'gsap'
 var objects = {}
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
 var PI2 = Math.PI * 2;
 var clock = new THREE.Clock();
 var t1 = new Date().getTime();
 var bindCanvas = (renderer) => {
+	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild(renderer.domElement);
 }
 
 function fsin(x) { //正弦函数
-	return 50 * Math.sin(0.8 * x * Math.PI / 180);
+	return 20 * Math.sin(0.8 * x * Math.PI / 180);
 }
 
 var sceneAdd = () => {
 	//画点
 	var pointsgroup = new THREE.Group();
 	scene.add(pointsgroup);
-	var geometry = new THREE.SphereGeometry(500, 50, 50);
+	var geometry = new THREE.SphereGeometry(50, 500, 500);
 	var vl = geometry.vertices.length;
 	for (var k = 0; k < vl; k++) {
 		//为每个点附上材质
@@ -41,20 +36,27 @@ var sceneAdd = () => {
 	}
 
 	var i = 0
+	try{
 	pointsgroup.traverse(function(child) {
 		var timerandom = 1 * Math.random();
+		if(i<=geometry.vertices.length){
 		TweenMax.to(
 			child.position,
 			timerandom, {
-				x: geometry.vertices[i].x + (0.5 - Math.random()) * 100,
-				y: geometry.vertices[i].y + (0.5 - Math.random()) * 100,
-				z: geometry.vertices[i].z + Math.random() * 100,
-				delay: 1
+				x: geometry.vertices[i]['x'] + (0.5 - Math.random()) * 100,
+				y: geometry.vertices[i]['y'] + (0.5 - Math.random()) * 100,
+				z: geometry.vertices[i]['z'] + Math.random() * 100,
+				delay: 3
 			}
 		);
 		i++;
+		}
 
 	});
+	}catch(error){ 
+		//console.log(error);
+		//发生错误执行的代码 
+	}
 
 
 	objects['pointsgroup'] = pointsgroup;
@@ -63,16 +65,17 @@ var sceneAdd = () => {
 var animate = () => {
 	requestAnimationFrame(animate);
 	var delta = 10 * clock.getDelta();
-	var speed = 10;
+	var speed = 2;
 	delta = delta < 2 ? delta : 2;
 	var dur = new Date().getTime() - t1;
-	if (dur < 500) { //控制飞线动画时间
+	if (dur < 5125) { //控制飞线动画时间
 		objects['pointsgroup'].traverse(function(child) {
 			if (child.position.y < window.innerHeight) {
-				child.position.y += delta * speed * Math.random();
-				child.position.x = fsin(child.position.y);
-				console.log("x" + child.position.x);
+				child.position.y += speed * Math.random()*0.5;
+				//child.position.x = fsin(child.position.y);
 			}
+			else
+				console.log("dur"+dur);
 		});
 	}
 
