02b770dae2010b5c0b161b719634a9474e7e1136
dingdang <weihagu_work@163.com>
2021-02-07 17:31:06 +0800

    oo

--- src/index.js
+++ src/index.js
@@ -1,65 +1,74 @@
 var objects = {}
-var vl = 3000;
+var vl = 1000;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer();
 var PI2 = Math.PI * 2;
 var clock = new THREE.Clock();
 var t1 = new Date().getTime();
 var bindCanvas = (renderer) => {
-	renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);//设置渲染区域尺寸
 	document.body.appendChild(renderer.domElement);
 }
 
 function fsin(x) { //正弦函数
-	return 20 * Math.sin(0.8 * x * Math.PI / 180);
+	return 10 * Math.sin(x);
 }
 
-var sceneAdd = () => {
-	//画点
+var addPoints = () =>{
+    //画点
 	var pointsgroup = new THREE.Group();
-	scene.add(pointsgroup);
-	var geometry = new THREE.SphereGeometry(20, 100, 100);
-	var gvl = geometry.vertices.length;
-	console.log(geometry.vertices);
-	var material2 = new THREE.MeshLambertMaterial({
-	 color: 0xff00ff
-	});
-	var mesh2 = new THREE.Mesh(geometry, material2); //网格模型对象Mesh
-	mesh2.translateY(120); //球体网格模型沿Y轴正方向平移120
-	scene.add(mesh2);
-	// 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
-	var axisHelper = new THREE.AxesHelper(250);
-	scene.add(axisHelper);
-	//geometry.position.y=10;
-	for (var k = 0; k < vl; k++) {
-		//为每个点附上材质
-		var geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
-		// 点渲染模式
-		var material = new THREE.PointsMaterial({
-			color: Math.random() * 0x808008 + 0x808080,
-			size: 0.2 //点对象像素尺寸
-		}); //材质对象
-		var particle = new THREE.Points(geometry, material); //点模型对象
-		particle.position.x = 0;
-		particle.position.y = -5;
-		particle.position.z = 0;
+    for (var k = 0; k < vl; k++) {
+		var geometry = new THREE.BoxGeometry(0.01, 0.01, 0.01); //创建一个立方体几何对象Geometry
+		var material = new THREE.MeshBasicMaterial( { color: Math.random() * 0x808008 + 0x808080 } );
+		var particle = new THREE.Mesh( geometry, material );; //点模型对象
+        particle.position.x=0;
+        particle.position.y=-10;
 		pointsgroup.add(particle);
 	}
+    scene.add(pointsgroup);
+    objects['pointsgroup']=pointsgroup;
+    console.log(pointsgroup);
 
-	try{
+    
+}
+
+var fallText = ()=>{
+    var loader = new THREE.FontLoader();
+
+    loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+	var geometry = new THREE.TextGeometry( 'Hello three.js!', {
+		font: font,
+		size: 80,
+		height: 5,
+		curveSegments: 12,
+		bevelEnabled: true,
+		bevelThickness: 10,
+		bevelSize: 8,
+		bevelSegments: 5
+	} );
+} );
+
+
+
+}
+
+var pointAnimate = (vertices) =>{
+    try{
 	var gi = 0;
-	pointsgroup.traverse(function(child) {
+	objects['pointsgroup'].traverse(function(child) {
 		var timerandom = 1 * Math.random();
-		var i=gi%gvl;
-		if(i<=geometry.vertices.length){
+		var i=gi%vertices.length;
+		if(i<=vertices.length){
 		TweenMax.to(
 			child.position,
 			timerandom, {
-				x: geometry.vertices[i]['x'] + (0.5 - Math.random()) * 100,
-				y: geometry.vertices[i]['y'] + (0.5 - Math.random()) * 100,
-				z: geometry.vertices[i]['z'] + Math.random() * 100,
-				delay: 2.5
+				x: vertices[i][x] + (0.5 - Math.random()) * 100,
+				y: vertices[i][y] + (0.5 - Math.random()) * 100,
+				z: vertices[i][z] + Math.random() * 100,
+				delay: 1
 			}
 		);
 		gi++;
@@ -70,29 +79,45 @@ var sceneAdd = () => {
 		//console.log(error);
 		//发生错误执行的代码 
 	}
+}
+    
 
+var sceneAdd = () => {
+    //fallText();
+    addPoints();
+    //pointAnimate(geometry.vertices);
 
-	objects['pointsgroup'] = pointsgroup;
-	objects['geometry'] = geometry;
 }
 var animate = () => {
 	requestAnimationFrame(animate);
-	var delta = 10 * clock.getDelta();
+	pointUpdate();
+	renderer.render(scene, camera);
+}
+
+var pointUpdate = () =>{
+    var delta = 10 * clock.getDelta();
 	var speed = 2;
 	delta = delta < 2 ? delta : 2;
 	var dur = new Date().getTime() - t1;
-	if (dur < 5125) { //控制飞线动画时间
+	if (dur < 2500) { //控制飞线动画时间
 		objects['pointsgroup'].traverse(function(child) {
 			if (child.position.y < window.innerHeight) {
-				child.position.y += speed * Math.random()*0.5;
-				//child.position.x = fsin(child.position.y);
+				child.position.y += speed * Math.random()*0.06;
+                
 			}
 			else
 				console.log("dur"+dur);
 		});
 	}
-
-	renderer.render(scene, camera);
+    if(dur >2500 && dur <3500){
+        objects['pointsgroup'].traverse(function(child) {
+				child.position.y -= fsin(child.position.y) * Math.random()*0.6;
+				child.position.x = fsin(child.position.y)*0.3*Math.random();
+                 child.position.z = fsin(child.position.x)*0.3*Math.random();
+			
+		});
+    }
+    
 }
 
 bindCanvas(renderer);
